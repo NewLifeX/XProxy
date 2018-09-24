cdad0de8ae487320aa8333c5940dfa854c9ca705
大石头 <nnhy@vip.qq.com>
2018-09-24 19:47:47 +0800

    使用说明

--- XProxy/Properties/AssemblyInfo.cs
+++ XProxy/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过下列属性集
 // 控制。更改这些属性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("XProxy代理库")]
+[assembly: AssemblyTitle("XProxy代理服务器")]
 [assembly: AssemblyDescription("反向代理")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("新生命开发团队")]
