63b54ff0db7340bd13bd97d5939ec35eae667772
大石头 <nnhy@vip.qq.com>
2022-09-09 14:26:22 +0800

    端口数。支持映射一批端口到目标服务器的对应端口上

--- XProxy/Proxy/HttpProxy.cs
+++ XProxy/Proxy/HttpProxy.cs
@@ -1,256 +1,252 @@
-﻿using System;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Net;
 using NewLife.Http;
 using NewLife.Net.Http;
 
-namespace NewLife.Net.Proxy
+namespace NewLife.Net.Proxy;
+
+/// <summary>Http代理。可用于代理各种Http通讯请求。</summary>
+/// <remarks>Http代理请求与普通请求唯一的不同就是Uri，Http代理请求收到的是可能包括主机名的完整Uri</remarks>
+public class HttpProxy : ProxyBase
 {
-    /// <summary>Http代理。可用于代理各种Http通讯请求。</summary>
-    /// <remarks>Http代理请求与普通请求唯一的不同就是Uri，Http代理请求收到的是可能包括主机名的完整Uri</remarks>
-    public class HttpProxy : ProxyBase
+    #region 属性
+    /// <summary>代理用户名</summary>
+    public String Username { get; set; }
+
+    /// <summary>代理密码</summary>
+    public String Password { get; set; }
+
+    /// <summary>对于被保护区域（即安全域）的描述。如果没有指定安全域，客户端通常用一个格式化的主机名来代替。</summary>
+    public String Realm { get; set; }
+    #endregion
+
+    #region 构造
+    /// <summary>实例化</summary>
+    public HttpProxy()
     {
-        #region 属性
-        /// <summary>代理用户名</summary>
-        public String Username { get; set; }
+        Port = 8080;
+        ProtocolType = NetType.Tcp;
+    }
+    #endregion
 
-        /// <summary>代理密码</summary>
-        public String Password { get; set; }
-
-        /// <summary>对于被保护区域（即安全域）的描述。如果没有指定安全域，客户端通常用一个格式化的主机名来代替。</summary>
-        public String Realm { get; set; }
-        #endregion
-
-        #region 构造
-        /// <summary>实例化</summary>
-        public HttpProxy()
+    #region 方法
+    public override void Init(String config)
+    {
+        var dic = config?.SplitAsDictionary("=", ";");
+        if (dic != null && dic.Count > 0)
         {
-            Port = 8080;
-            ProtocolType = NetType.Tcp;
+            Username = dic["user"];
+            Password = dic["pass"];
+            Realm = dic["realm"];
         }
-        #endregion
+    }
+    #endregion
 
-        #region 方法
-        public override void Init(String config)
+    #region 会话
+    /// <summary>创建会话</summary>
+    /// <param name="session"></param>
+    /// <returns></returns>
+    protected override INetSession CreateSession(ISocketSession session) => new Session();
+
+    /// <summary>Http反向代理会话</summary>
+    public class Session : ProxySession
+    {
+        /// <summary>已完成处理，正在转发数据的请求头</summary>
+        public HttpHeader Request { get; set; }
+
+        /// <summary>已认证</summary>
+        public Boolean Authenticated { get; set; }
+
+        /// <summary>收到客户端发来的数据。子类可通过重载该方法来修改数据</summary>
+        /// <remarks>
+        /// 如果数据包包括头部和主体，可以分开处理。
+        /// 最麻烦的就是数据包不是一个完整的头部，还落了一部分在后面的包上。
+        /// </remarks>
+        /// <param name="e"></param>
+        protected override void OnReceive(ReceivedEventArgs e)
         {
-            var dic = config?.SplitAsDictionary("=", ";");
-            if (dic != null && dic.Count > 0)
+            if (e.Packet.Total == 0 || !HttpBase.FastValidHeader(e.Packet))
             {
-                Username = dic["user"];
-                Password = dic["pass"];
-                Realm = dic["realm"];
-            }
-        }
-        #endregion
-
-        #region 会话
-        /// <summary>创建会话</summary>
-        /// <param name="session"></param>
-        /// <returns></returns>
-        protected override INetSession CreateSession(ISocketSession session) => new Session();
-
-        /// <summary>Http反向代理会话</summary>
-        public class Session : ProxySession
-        {
-            /// <summary>已完成处理，正在转发数据的请求头</summary>
-            public HttpHeader Request { get; set; }
-
-            /// <summary>已认证</summary>
-            public Boolean Authenticated { get; set; }
-
-            /// <summary>收到客户端发来的数据。子类可通过重载该方法来修改数据</summary>
-            /// <remarks>
-            /// 如果数据包包括头部和主体，可以分开处理。
-            /// 最麻烦的就是数据包不是一个完整的头部，还落了一部分在后面的包上。
-            /// </remarks>
-            /// <param name="e"></param>
-            protected override void OnReceive(ReceivedEventArgs e)
-            {
-                if (e.Packet.Total == 0 || !HttpBase.FastValidHeader(e.Packet))
-                {
-                    base.OnReceive(e);
-                    return;
-                }
-
-                var request = new HttpRequest();
-                if (request.Parse(e.Packet))
-                {
-                    WriteLog("[{0}]{1} {2}", Remote.Address, request.Method, request.Url);
-
-                    using var span = Host.Tracer?.NewSpan("proxy:HttpProxyRequest", request.Url + "");
-
-                    if (!OnRequest(request, e)) return;
-
-                    e.Packet = request.Build();
-                }
-
                 base.OnReceive(e);
+                return;
             }
 
-            /// <summary>收到请求时</summary>
-            /// <param name="request"></param>
-            /// <param name="e"></param>
-            /// <returns></returns>
-            protected virtual Boolean OnRequest(HttpRequest request, ReceivedEventArgs e)
+            var request = new HttpRequest();
+            if (request.Parse(e.Packet))
             {
-                var proxy = (this as INetSession).Host as HttpProxy;
+                WriteLog("[{0}]{1} {2}", Remote.Address, request.Method, request.Url);
 
-                // 代理认证
-                if (!proxy.Username.IsNullOrEmpty() && !OnAuthenticate(request)) return false;
+                using var span = Host.Tracer?.NewSpan("proxy:HttpProxyRequest", request.Url + "");
 
-                // https使用CONNECT建立连接
-                if (request.Method.EqualIgnoreCase("CONNECT")) return ProcessConnect(request, e);
+                if (!OnRequest(request, e)) return;
 
-                var remote = RemoteServer;
-                var ruri = RemoteServerUri;
-                if (request.Url.IsAbsoluteUri)
-                {
-                    var uri = request.Url;
-                    var host = uri.Host + ":" + uri.Port;
-
-                    // 可能不含Host
-                    if (request.Host.IsNullOrEmpty()) request.Host = host;
-
-                    // 如果地址或端口改变，则重新连接服务器
-                    if (remote != null && (uri.Host != ruri.Host || uri.Port != ruri.Port))
-                    {
-                        remote.Dispose();
-                        RemoteServer = null;
-                    }
-
-                    ruri.Host = uri.Host;
-                    ruri.Port = uri.Port;
-                    request.Url = new Uri(uri.PathAndQuery, UriKind.Relative);
-                }
-                else if (!request.Host.IsNullOrEmpty())
-                {
-                    ruri.Host = request.Host;
-                    ruri.Port = 80;
-                }
-                else
-                    throw new XException("无法处理的请求！{0}", request);
-
-                // 处理KeepAlive
-                if (request.Headers.TryGetValue("Proxy-Connection", out var str))
-                {
-                    request.KeepAlive = str.EqualIgnoreCase("keep-alive");
-                    request.Headers.Remove("Proxy-Connection");
-                }
-
-                // 移除所有代理专用头部字段，避免传递给网站服务器
-                var keys = request.Headers.Where(e => e.Key.StartsWithIgnoreCase("proxy-")).ToArray();
-                foreach (var item in keys)
-                {
-                    request.Headers.Remove(item);
-                }
-
-                return true;
+                e.Packet = request.Build();
             }
 
-            /// <summary>代理认证</summary>
-            /// <param name="request"></param>
-            /// <returns></returns>
-            protected virtual Boolean OnAuthenticate(HttpRequest request)
+            base.OnReceive(e);
+        }
+
+        /// <summary>收到请求时</summary>
+        /// <param name="request"></param>
+        /// <param name="e"></param>
+        /// <returns></returns>
+        protected virtual Boolean OnRequest(HttpRequest request, ReceivedEventArgs e)
+        {
+            var proxy = (this as INetSession).Host as HttpProxy;
+
+            // 代理认证
+            if (!proxy.Username.IsNullOrEmpty() && !OnAuthenticate(request)) return false;
+
+            // https使用CONNECT建立连接
+            if (request.Method.EqualIgnoreCase("CONNECT")) return ProcessConnect(request, e);
+
+            var remote = RemoteServer;
+            var ruri = RemoteServerUri;
+            if (request.Url.IsAbsoluteUri)
             {
-                if (Authenticated) return true;
+                var uri = request.Url;
+                var host = uri.Host + ":" + uri.Port;
 
-                var proxy = (this as INetSession).Host as HttpProxy;
+                // 可能不含Host
+                if (request.Host.IsNullOrEmpty()) request.Host = host;
 
-                var token = request.Headers["Proxy-Authorization"];
-                if (!token.IsNullOrEmpty() && token.StartsWithIgnoreCase("Basic "))
+                // 如果地址或端口改变，则重新连接服务器
+                if (remote != null && (uri.Host != ruri.Host || uri.Port != ruri.Port))
                 {
-                    token = token.Substring("Basic ".Length);
-                    token = token.ToBase64().ToStr();
-
-                    var p = token.IndexOf(':');
-                    if (p > 0 && p < token.Length - 1)
-                    {
-                        var user = token.Substring(0, p);
-                        var pass = token.Substring(p + 1);
-                        if (user == proxy.Username && pass == proxy.Password)
-                        {
-                            Authenticated = true;
-                        }
-                    }
+                    remote.Dispose();
+                    RemoteServer = null;
                 }
 
-                if (!Authenticated)
-                {
-                    var rs = new HttpResponse
-                    {
-                        Version = request.Version,
-                        StatusCode = HttpStatusCode.ProxyAuthenticationRequired,
-                    };
-
-                    rs.Headers["Proxy-Authenticate"] = $"Basic realm=\"{proxy.Realm}\"";
-
-                    Send(rs.Build());
-
-                    return false;
-                }
-
-                return true;
-            }
-
-            private Boolean ProcessConnect(HttpRequest request, ReceivedEventArgs e)
-            {
-                using var span = Host.Tracer?.NewSpan("proxy:HttpProxyConnect", request.Url + "");
-
-                var pxy = Host as HttpProxy;
-
-                var uri = new NetUri(request.Url.ToString());
-                var ruri = RemoteServerUri;
                 ruri.Host = uri.Host;
                 ruri.Port = uri.Port;
+                request.Url = new Uri(uri.PathAndQuery, UriKind.Relative);
+            }
+            else if (!request.Host.IsNullOrEmpty())
+            {
+                ruri.Host = request.Host;
+                ruri.Port = 80;
+            }
+            else
+                throw new XException("无法处理的请求！{0}", request);
 
-                // 不要连自己，避免死循环
-                if (uri.Port == pxy.Server.Port && uri.Address.IsLocal())
+            // 处理KeepAlive
+            if (request.Headers.TryGetValue("Proxy-Connection", out var str))
+            {
+                request.KeepAlive = str.EqualIgnoreCase("keep-alive");
+                request.Headers.Remove("Proxy-Connection");
+            }
+
+            // 移除所有代理专用头部字段，避免传递给网站服务器
+            var keys = request.Headers.Where(e => e.Key.StartsWithIgnoreCase("proxy-")).ToArray();
+            foreach (var item in keys)
+            {
+                request.Headers.Remove(item);
+            }
+
+            return true;
+        }
+
+        /// <summary>代理认证</summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        protected virtual Boolean OnAuthenticate(HttpRequest request)
+        {
+            if (Authenticated) return true;
+
+            var proxy = (this as INetSession).Host as HttpProxy;
+
+            var token = request.Headers["Proxy-Authorization"];
+            if (!token.IsNullOrEmpty() && token.StartsWithIgnoreCase("Basic "))
+            {
+                token = token.Substring("Basic ".Length);
+                token = token.ToBase64().ToStr();
+
+                var p = token.IndexOf(':');
+                if (p > 0 && p < token.Length - 1)
                 {
-                    WriteLog("不要连自己，避免死循环");
-                    Dispose();
-                    return false;
+                    var user = token.Substring(0, p);
+                    var pass = token.Substring(p + 1);
+                    if (user == proxy.Username && pass == proxy.Password)
+                    {
+                        Authenticated = true;
+                    }
                 }
+            }
 
-                var rs = new HttpResponse { Version = request.Version };
-                try
+            if (!Authenticated)
+            {
+                var rs = new HttpResponse
                 {
-                    // 连接远程服务器，启动数据交换
-                    if (RemoteServer == null) ConnectRemote(e);
+                    Version = request.Version,
+                    StatusCode = HttpStatusCode.ProxyAuthenticationRequired,
+                };
 
-                    // 响应头增加所使用的本地IP地址，让客户端知道
-                    rs.Headers["Proxy-Local-Ip"] = RemoteServer?.Local.Address + "";
+                rs.Headers["Proxy-Authenticate"] = $"Basic realm=\"{proxy.Realm}\"";
 
-                    rs.StatusCode = HttpStatusCode.OK;
-                    rs.StatusDescription = "OK";
-                }
-#if NET5_0
-                catch (HttpRequestException ex)
-                {
-                    rs.StatusCode = ex.StatusCode ?? HttpStatusCode.BadRequest;
-                    rs.StatusDescription = ex.Message;
-                }
-#endif
-                catch (Exception ex)
-                {
-                    rs.StatusCode = HttpStatusCode.BadGateway;
-                    rs.StatusDescription = ex.Message;
-                }
-
-                // 告诉客户端，已经连上了服务端，或者没有连上，这里不需要向服务端发送任何数据
                 Send(rs.Build());
 
                 return false;
             }
 
-            /// <summary>远程连接断开时触发。默认销毁整个会话，子类可根据业务情况决定客户端与代理的链接是否重用。</summary>
-            /// <param name="session"></param>
-            protected override void OnRemoteDispose(ISocketClient session)
-            {
-                // 如果客户端不要求保持连接，就销毁吧
-                if (Request != null && !Request.KeepAlive) base.OnRemoteDispose(session);
-            }
+            return true;
         }
-        #endregion
+
+        private Boolean ProcessConnect(HttpRequest request, ReceivedEventArgs e)
+        {
+            using var span = Host.Tracer?.NewSpan("proxy:HttpProxyConnect", request.Url + "");
+
+            var pxy = Host as HttpProxy;
+
+            var uri = new NetUri(request.Url.ToString());
+            var ruri = RemoteServerUri;
+            ruri.Host = uri.Host;
+            ruri.Port = uri.Port;
+
+            // 不要连自己，避免死循环
+            if (uri.Port == pxy.Server.Port && uri.Address.IsLocal())
+            {
+                WriteLog("不要连自己，避免死循环");
+                Dispose();
+                return false;
+            }
+
+            var rs = new HttpResponse { Version = request.Version };
+            try
+            {
+                // 连接远程服务器，启动数据交换
+                if (RemoteServer == null) ConnectRemote(e);
+
+                // 响应头增加所使用的本地IP地址，让客户端知道
+                rs.Headers["Proxy-Local-Ip"] = RemoteServer?.Local.Address + "";
+
+                rs.StatusCode = HttpStatusCode.OK;
+                rs.StatusDescription = "OK";
+            }
+#if NET5_0
+            catch (HttpRequestException ex)
+            {
+                rs.StatusCode = ex.StatusCode ?? HttpStatusCode.BadRequest;
+                rs.StatusDescription = ex.Message;
+            }
+#endif
+            catch (Exception ex)
+            {
+                rs.StatusCode = HttpStatusCode.BadGateway;
+                rs.StatusDescription = ex.Message;
+            }
+
+            // 告诉客户端，已经连上了服务端，或者没有连上，这里不需要向服务端发送任何数据
+            Send(rs.Build());
+
+            return false;
+        }
+
+        /// <summary>远程连接断开时触发。默认销毁整个会话，子类可根据业务情况决定客户端与代理的链接是否重用。</summary>
+        /// <param name="session"></param>
+        protected override void OnRemoteDispose(ISocketClient session)
+        {
+            // 如果客户端不要求保持连接，就销毁吧
+            if (Request != null && !Request.KeepAlive) base.OnRemoteDispose(session);
+        }
     }
+    #endregion
 }
--- XProxy/Proxy/NATProxy.cs
+++ XProxy/Proxy/NATProxy.cs
@@ -1,55 +1,91 @@
-﻿using System;
+﻿namespace NewLife.Net.Proxy;
 
-namespace NewLife.Net.Proxy
+/// <summary>通用NAT代理。所有收到的数据，都转发到指定目标</summary>
+public class NATProxy : ProxyBase
 {
-    /// <summary>通用NAT代理。所有收到的数据，都转发到指定目标</summary>
-    public class NATProxy : ProxyBase
+    #region 属性
+    /// <summary>远程服务器地址</summary>
+    public NetUri RemoteServer { get; set; } = new NetUri();
+
+    /// <summary>端口数。支持映射一批端口到目标服务器的对应端口上</summary>
+    public Int32 Ports { get; set; }
+    #endregion
+
+    #region 构造
+    /// <summary>实例化</summary>
+    public NATProxy() { }
+
+    /// <summary>实例化</summary>
+    /// <param name="hostname">目标服务器地址</param>
+    /// <param name="port">目标服务器端口</param>
+    public NATProxy(String hostname, Int32 port) : this(hostname, port, NetType.Tcp) { }
+
+    /// <summary>实例化</summary>
+    /// <param name="hostname">目标服务器地址</param>
+    /// <param name="port">目标服务器端口</param>
+    /// <param name="protocol">协议</param>
+    public NATProxy(String hostname, Int32 port, NetType protocol)
+        : this() => RemoteServer = new NetUri(protocol, hostname, port);
+    #endregion
+
+    #region 方法
+    public override void Init(String config)
     {
-        #region 属性
-        /// <summary>远程服务器地址</summary>
-        public NetUri RemoteServer { get; set; } = new NetUri();
-        #endregion
-
-        #region 构造
-        /// <summary>实例化</summary>
-        public NATProxy() { }
-
-        /// <summary>实例化</summary>
-        /// <param name="hostname">目标服务器地址</param>
-        /// <param name="port">目标服务器端口</param>
-        public NATProxy(String hostname, Int32 port) : this(hostname, port, NetType.Tcp) { }
-
-        /// <summary>实例化</summary>
-        /// <param name="hostname">目标服务器地址</param>
-        /// <param name="port">目标服务器端口</param>
-        /// <param name="protocol">协议</param>
-        public NATProxy(String hostname, Int32 port, NetType protocol)
-            : this() => RemoteServer = new NetUri(protocol, hostname, port);
-        #endregion
-
-        #region 方法
-        /// <summary>开始</summary>
-        protected override void OnStart()
+        var dic = config?.SplitAsDictionary("=", ";");
+        if (dic != null && dic.Count > 0)
         {
-            WriteLog("NAT代理 => {0}", RemoteServer);
-
-            if (RemoteServer.Type == 0) RemoteServer.Type = ProtocolType;
-
-            base.OnStart();
+            Ports = dic["Ports"].ToInt();
         }
-
-        /// <summary>添加会话。子类可以在添加会话前对会话进行一些处理</summary>
-        /// <param name="session"></param>
-        protected override void AddSession(INetSession session)
-        {
-            var ps = session as ProxySession;
-            ps.RemoteServerUri = RemoteServer;
-            // 如果不是Tcp/Udp，则使用本地协议
-            if (!RemoteServer.IsTcp && !RemoteServer.IsUdp)
-                ps.RemoteServerUri.Type = Local.Type;
-
-            base.AddSession(session);
-        }
-        #endregion
     }
+
+    /// <summary>开始</summary>
+    protected override void OnStart()
+    {
+        WriteLog("NAT代理 => {0}", RemoteServer);
+
+        if (RemoteServer.Type == 0) RemoteServer.Type = ProtocolType;
+
+        // 多端口
+        if (Ports > 1)
+        {
+            for (var i = 0; i < Ports; i++)
+            {
+                var list = CreateServer(Local.Address, Port + i, Local.Type, AddressFamily);
+                foreach (var item in list)
+                {
+                    AttachServer(item);
+                }
+            }
+        }
+
+        base.OnStart();
+    }
+
+    /// <summary>添加会话。子类可以在添加会话前对会话进行一些处理</summary>
+    /// <param name="session"></param>
+    protected override void AddSession(INetSession session)
+    {
+        var svr = RemoteServer;
+
+        // 多端口
+        if (Ports > 1)
+        {
+            // 算出来当前端口的索引位置
+            var port = session.Session.Local.Port;
+            var index = port - Port;
+
+            // 计算目标端口
+            svr = new NetUri(svr.ToString());
+            svr.Port += index;
+        }
+
+        var ps = session as ProxySession;
+        ps.RemoteServerUri = svr;
+        // 如果不是Tcp/Udp，则使用本地协议
+        if (!RemoteServer.IsTcp && !RemoteServer.IsUdp)
+            ps.RemoteServerUri.Type = Local.Type;
+
+        base.AddSession(session);
+    }
+    #endregion
 }
